517332132ac822c5891ec9dca32ed9829a186fb2
João Pedro Pimenta <jppimenta.pessoal@gmail.com>
2019-08-27 11:52:58 -0300

    Initial commit
    My life would've been easier if this was on github months ago. Better late than never.

---
+++ main.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+const token = "NTYwMjYxNzAxMTIwMDMyNzc4.XVynXg.QHTmkz1v-a7yurFCiXorMSUkRqk";
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("I'm now connected to this bitch ass server of yours!.");
+});
+
+client.on('message', (msg) => {
+    if (msg.content === '1') {
+        if (msg.member.voiceChannel) {
+            msg.channel.send('To colando!')
+            msg.member.voiceChannel.join()
+            .then(connection => {
+                const dispatcher = connection.playFile("C:/Users/Pimenta/Documents/aoeBot/taunts/01-yes.mp3");
+            }).catch(err => {
+                console.log('~~~DEU RUIM PATRÃO~~~');
+                console.log(err);
+            });
+        }
+        else {
+            msg.channel.send(`You must be in a Voice Channel for me to talk to you, ${msg.author.username}!`);
+        }
+   }
+ });
+
+client.login(token);
